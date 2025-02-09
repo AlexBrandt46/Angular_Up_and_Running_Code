025ca288b3a6278459d34a4c4c6f938bb611aa92
Alex Brandt <ajb3155@gmail.com>
2025-02-09 16:38:42 -0600

    Added basic layout for stock-component

--- Ch_2/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ Ch_2/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -7,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './stock-item.component.scss'
 })
 export class StockItemComponent implements OnInit {
-  constructor() {
 
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+
+  // TODO: Research why TypeScript threw errors when leaving variables uninitialized.
+  constructor() {
+    this.name = '';
+    this.code = '';
+    this.price = 0;
+    this.previousPrice = 0;
   }
 
   ngOnInit(): void {
-    
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
   }
 }
