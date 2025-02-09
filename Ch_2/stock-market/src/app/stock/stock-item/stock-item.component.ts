43739e81a337baabcab3ace1e8ee54145a790cbc
Alex Brandt <ajb3155@gmail.com>
2025-02-09 16:55:59 -0600

    Add in event binding (click) and property binding [disabled] for favoriting a stock item

--- Ch_2/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ Ch_2/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -12,6 +12,7 @@ export class StockItemComponent implements OnInit {
   public price: number;
   public previousPrice: number;
   public positiveChange: boolean;
+  public favorite: boolean;
 
   // TODO: Research why TypeScript threw errors when leaving variables uninitialized.
   constructor() {
@@ -20,13 +21,20 @@ export class StockItemComponent implements OnInit {
     this.price = 0;
     this.previousPrice = 0;
     this.positiveChange = false;
+    this.favorite = false;
   }
 
   ngOnInit(): void {
     this.name = 'Test Stock Company';
     this.code = 'TSC';
     this.price = 85;
-    this.previousPrice = 80;
+    this.previousPrice = 90;
     this.positiveChange = this.price >= this.previousPrice;
+    this.favorite = false;
+  }
+
+  toggleFavorite(): void {
+    console.log('We are toggling the favorite state for this stock.');
+    this.favorite = !this.favorite;
   }
 }
