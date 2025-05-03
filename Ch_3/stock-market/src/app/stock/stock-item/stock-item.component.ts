9bb119725891deede4a7c55b66da3db3aa99f4c5
Alex Brandt <ajb3155@gmail.com>
2025-05-03 11:12:35 -0500

    Reverted to use class directive instead of style

--- Ch_3/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ Ch_3/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -10,21 +10,23 @@ import { Stock } from '../../model/stock'
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
-  public stockStyles;
+  public stockClasses;
 
   // TODO: Research why TypeScript threw errors when leaving variables uninitialized.
   constructor() {
     this.stock = new Stock('', '', 0, 0)
-    this.stockStyles = {}
+    this.stockClasses = {}
   }
 
   ngOnInit(): void {
     this.stock = new Stock('Test Stock Company', 'TSC', 90, 85);
     let diff = (this.stock.price / this.stock.previousPrice) - 1;
     let largeChange = Math.abs(diff) > 0.01;
-    this.stockStyles = {
-      "color": this.stock.isPositiveChange() ? "green" : "red",
-      "font-size": largeChange ? "1.2em" : "0.8em"
+    this.stockClasses = {
+      "positive": this.stock.isPositiveChange(),
+      "negative": !this.stock.isPositiveChange(),
+      "large-change": largeChange,
+      "small-change": !largeChange
     };
   }
 
