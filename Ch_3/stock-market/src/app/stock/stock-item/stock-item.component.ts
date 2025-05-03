34395c6e40a79f1751f8c5a6ee98a9411f93ba2f
Alex Brandt <ajb3155@gmail.com>
2025-05-03 11:42:17 -0500

    Added @for block to display list of stocks

--- Ch_3/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ Ch_3/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -9,29 +9,36 @@ import { Stock } from '../../model/stock'
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
-  public stockClasses;
+  //public stock: Stock;
+  public stocks: Array<Stock>;
+  //public stockClasses;
 
   // TODO: Research why TypeScript threw errors when leaving variables uninitialized.
   constructor() {
-    this.stock = new Stock('', '', 0, 0)
-    this.stockClasses = {}
+    // this.stock = new Stock('', '', 0, 0)
+    // this.stockClasses = {}
+
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 90, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765)
+    ]
   }
 
   ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company', 'TSC', 90, 85);
-    let diff = (this.stock.price / this.stock.previousPrice) - 1;
-    let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      "positive": this.stock.isPositiveChange(),
-      "negative": !this.stock.isPositiveChange(),
-      "large-change": largeChange,
-      "small-change": !largeChange
-    };
+    // this.stock = new Stock('Test Stock Company', 'TSC', 90, 85);
+    // let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    // let largeChange = Math.abs(diff) > 0.01;
+    // this.stockClasses = {
+    //   "positive": this.stock.isPositiveChange(),
+    //   "negative": !this.stock.isPositiveChange(),
+    //   "large-change": largeChange,
+    //   "small-change": !largeChange
+    // };
   }
 
-  toggleFavorite(event: MouseEvent): void {
-    console.log('We are toggling the favorite state for this stock.', event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event: MouseEvent, index: number): void {
+    console.log('We are toggling the favorite state for this stock.', index, event);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 }
