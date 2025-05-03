8162baf27536bcc742256574683ac80be3ab55f3
Alex Brandt <ajb3155@gmail.com>
2025-05-03 18:36:34 -0500

    Completed ch3 exercise to add multiple components, use class binding, and add a quantity dropdown

--- Ch_3/ecommerce/src/app/product/product/product.component.ts
+++ Ch_3/ecommerce/src/app/product/product/product.component.ts
@@ -10,24 +10,47 @@ import { Product } from '../../model/product'
 })
 export class ProductComponent implements OnInit {
 
-  public product: Product;
+  public products: Array<Product>;
+  public productClasses: Array<{}>;
+  public quantOptions: Array<number>;
 
   constructor() {
-    this.product = new Product('', 0, '', false);
+    this.products = [];
+    this.productClasses = [];
+    this.quantOptions = [];
+
+    for (let i: number = 1; i <= 20; i++) {
+      this.quantOptions.push(i);
+    }
+
+    for (let i: number = 1; i <= 5; i++) {
+      let cost: number = this.getRandomNumberInRange(1, 100);
+      let onSale: boolean = this.getRandomNumberInRange(0, 2) % 2 == 0;
+      this.products.push(new Product(i, `Thingie ${i}`, cost, 'assets/item.jpg', onSale));
+      this.productClasses.push({
+        "onSaleBackground": onSale,
+        "regularPriceBackground": !onSale
+      });
+    }
+  }
+
+  private getRandomNumberInRange(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   ngOnInit(): void {
-    this.product = new Product('Thingie', 55, 'assets/item.jpg', true);
+
   }
 
-  changeCount(event: MouseEvent, increase: boolean) : void {
+  changeCount(event: MouseEvent, index: number, increase: boolean) : void {
+    console.log(increase);
     if (increase) {
-      console.log('We are increasing the amount of product in the cart.', event);
-      this.product.quantity++;
+      console.log('We are increasing the amount of product in the cart.', index, event);
+      this.products[index].quantity++;
     }
     else {
-      console.log('We are increasing the amount of product in the cart.', event);
-      this.product.quantity--;
+      console.log('We are decreasing the amount of product in the cart.', index, event);
+      this.products[index].quantity--;
     }
   }
 }
