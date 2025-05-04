4855f1cd1facc952b4160864403041983069d4cb
Alex Brandt <ajb3155@gmail.com>
2025-05-04 13:32:12 -0500

    Added input signals for passing Stock to stock-item component

--- Ch_4/stock-market/src/app/app.component.ts
+++ Ch_4/stock-market/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StockItemComponent } from "./stock/stock-item/stock-item.component";
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,26 @@ import { StockItemComponent } from "./stock/stock-item/stock-item.component";
   styleUrl: './app.component.scss',
   imports: [StockItemComponent]
 })
-export class AppComponent {
-  title = 'stock-market';
+export class AppComponent implements OnInit {
+  title = 'Stock Market App';
+
+  public stocks: Array<Stock> = [];
+  public stock: Stock;
+
+  constructor() {
+    console.log("constructor1")
+
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 90, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765)
+    ]
+
+    this.stock = this.stocks[0];
+  }
+
+  ngOnInit(): void {
+    console.log("init")
+    
+  }
 }
